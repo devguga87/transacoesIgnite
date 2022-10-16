e0baf85fed8aa56b493236ec5d2c7d9beeb746eb
gustavo.seabra <devguga87@gmail.com>
2022-10-15 22:50:37 -0300

    faz o fetch dos dados mockados

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react"
 import Header from "../../components/Header"
 import Summary from "../../components/Summary"
 import SearchForm from "./components/SearchForm"
 
 import * as Styled from './styles'
 
+interface Transaction {
+  id:number;
+  description:string;
+  type:'income'|'outcome'
+  price:number;
+  category:string;
+  createdAt:string;
+}
+
 const Transactions = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3333/transactions')
+    .then(response => response.json())
+    .then(data => setTransactions(data))
+  },[])
+
   return (
     <div>
       <Header />
@@ -13,20 +31,16 @@ const Transactions = () => {
       <SearchForm />
       <Styled.TransactionsTable>
         <tbody>
-          <tr>
-            <td width="50%">Desenvolvimento do site</td>
-            <td><Styled.PriceHighlight variant="income">R$ 12.000,00</Styled.PriceHighlight></td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-
-          <tr>
-            <td width="50%">Hamburguer</td>
-            <td><Styled.PriceHighlight variant="outcome">- R$ 54,00</Styled.PriceHighlight></td>
-            <td>Alimentação</td>
-            <td>13/04/2022</td>
-          </tr>
-
+          {transactions.map( transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td><Styled.PriceHighlight variant={transaction.type}>R${transaction.price}</Styled.PriceHighlight></td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            )
+          })}
          
         </tbody>
       </Styled.TransactionsTable>
