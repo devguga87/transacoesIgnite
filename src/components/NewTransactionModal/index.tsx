dd9c798e8bf6de0da9827e6ce73a6dbb020c69c1
gustavo.seabra <devguga87@gmail.com>
2022-10-16 22:37:36 -0300

    react-hook-form e zod

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,34 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { useForm } from "react-hook-form";
 
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 import * as Styled from "./styles";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(["income", "outcome"]),
+});
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 const NewTransactionModal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  });
+
+  function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
       <Styled.Overlay></Styled.Overlay>
@@ -12,28 +36,44 @@ const NewTransactionModal = () => {
         <Dialog.Title>Nova Transação</Dialog.Title>
 
         <Styled.CloseButton>
-          <X size={24}/>
+          <X size={24} />
         </Styled.CloseButton>
 
-        <form action="">
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register("description")}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register("price", { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register("category")}
+          />
 
           <Styled.TransactionType>
-            <Styled.TransactionTypeButton variant="income" value='income'>
-              <ArrowCircleUp size={24}/>
+            <Styled.TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
               Entrada
             </Styled.TransactionTypeButton>
 
-            <Styled.TransactionTypeButton variant='outcome' value='outcome'>
-              <ArrowCircleDown size={24}/>
+            <Styled.TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
               Saída
             </Styled.TransactionTypeButton>
-
           </Styled.TransactionType>
 
-          <button type="submit">Cadastra</button>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastra
+          </button>
         </form>
       </Styled.Content>
     </Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,16 +1,43 @@
-import { MagnifyingGlass } from 'phosphor-react'
-import * as Styled from './styles'
+import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import * as Styled from "./styles";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFormSchema = z.object({
+  query: z.string(),
+});
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 const SearchForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  });
+
+  function handleSearchTransactions(data: SearchFormInputs) {
+    console.log(data);
+  }
+
   return (
-    <Styled.SearchFormContainer>
-      <input type="text" placeholder='Busque uma transação'/>
-      <button type='submit'>
-        <MagnifyingGlass size={20}/>
+    <Styled.SearchFormContainer
+      onSubmit={handleSubmit(handleSearchTransactions)}
+    >
+      <input
+        type="text"
+        placeholder="Busque uma transação"
+        {...register("query")}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
         Buscar
       </button>
     </Styled.SearchFormContainer>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
